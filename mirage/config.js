fbe789c028d170e4198d29abb87275a3ad0179e7
bekzod <bekzod@me.com>
2017-04-04 11:26:49 +0500

    added ember-mirage and configured it

---
+++ mirage/config.js
@@ -0,0 +1,11 @@
+export default function() {
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  this.namespace = '';
+  this.get('/rentals');
+  this.get('/bookings');
+    // this.post('/posts');
+    // this.get('/posts/:id');
+    // this.put('/posts/:id'); // or this.patch
+    // this.del('/posts/:id');
+}
