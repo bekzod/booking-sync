5138984628420d19d25661277987ad1e1f889e3f
bekzod <bekzod@me.com>
2017-04-04 11:35:14 +0500

    namespaced api

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-
+  namespace: 'api',
   // headers: Ember.computed('session.authToken',  function () {
   //   return {
   //     'x-authentication': this.get('session.authToken')
--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,7 @@
 export default function() {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-  this.namespace = '';
+  this.namespace = '/api';
   this.get('/rentals');
   this.get('/bookings');
     // this.post('/posts');
