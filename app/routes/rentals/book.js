795044ae4198c2755fb150f5dc7d756e58a67fb1
bekzod <bekzod@me.com>
2017-04-04 15:31:36 +0500

    added cancel button for booking modal

--- app/routes/rentals/book.js
+++ app/routes/rentals/book.js
@@ -10,6 +10,15 @@ export default Ember.Route.extend({
     let booking = this.get('store').createRecord('booking', { rental })
     controller.set('rental', rental)
     controller.set('booking', booking)
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      let booking = controller.get('booking')
+      if (booking && booking.get('isNew')) {
+        booking.destroyRecord()
+      }
+    }
   }
 
 });
