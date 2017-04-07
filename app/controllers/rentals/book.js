23888a97bc8d552971c9ee9d0de4b54d1059bedb
bekzod <bekzod@me.com>
2017-04-07 21:41:19 +0500

    added styling for half booked days

--- app/controllers/rentals/book.js
+++ app/controllers/rentals/book.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 import { animate } from 'ember-animatable';
 
 
-const { computed, inject } = Ember
+const { computed, inject, run, $ } = Ember
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
@@ -12,10 +12,10 @@ export default Ember.Controller.extend({
 
   minDate: moment().startOf('day'),
   //centering to first available day
-  center: computed('disabledDates', {
+  center: computed('disabledDates.dates', {
     get() {
       let center = moment()
-      let disabledDates = this.get('disabledDates')
+      let disabledDates = this.get('disabledDates.dates')
       for (var i = 0; i < disabledDates.length; i++) {
         let disabledDate = disabledDates[i]
         if (center.isSame(disabledDate, 'day')) {
@@ -62,7 +62,7 @@ export default Ember.Controller.extend({
     return !bookings.any(function(booking) {
       let bookingStart = booking.get('startAt')
       let bookingEnd = booking.get('endAt')
-      return startAt.isBefore(bookingStart) && endAt.isAfter(bookingEnd)
+      return (startAt.isBefore(bookingStart) || startAt.isSame(bookingStart, 'day')) && endAt.isAfter(bookingStart)
     })
   },
 
@@ -70,17 +70,63 @@ export default Ember.Controller.extend({
     return this.get('rental.bookings').filter((booking)=> !booking.get('isNew'))
   }),
 
+  // big hack
+  drawCalendarItems() {
+    let bookHash = this.get('disabledDates.bookHash')
+    for (let key in bookHash) {
+      let { afterTwelve, beforeTwelve, fullDay } = bookHash[key]
+        let el = $(`[data-date="${key}"]`)
+        if (fullDay) {
+          el.addClass('full-day')
+        } else if (afterTwelve) {
+          el.addClass('second-half')
+        } else if (beforeTwelve) {
+          el.addClass('first-half')
+        }
+    }
+  },
+
   disabledDates: computed('bookings', function() {
-    return this.get('bookings').reduce(function(dates, booking) {
+    let bookHash = {}
+    let dates = this.get('bookings').reduce(function(dates, booking) {
       let startAt = booking.get('startAt')
       let endAt = booking.get('endAt')
       let date = moment(startAt)
+
+      let startKey = date.format('YYYY-MM-DD')
+      bookHash[startKey] = bookHash[startKey] || {}
+      let obj1 = bookHash[startKey]
+      if (obj1.beforeTwelve) {
+        dates.push(date.toDate())
+        bookHash[date.format('YYYY-MM-DD')] = { fullDay: true }
+      } else {
+        obj1.afterTwelve = true
+      }
+
+      let endKey = moment(endAt).format('YYYY-MM-DD')
+      bookHash[endKey] = bookHash[endKey] || {}
+      let obj2 = bookHash[endKey]
+      if (obj2.afterTwelve) {
+        let mDate = moment(endAt)
+        dates.push(mDate.toDate())
+        bookHash[mDate.format('YYYY-MM-DD')] = { fullDay: true }
+      } else {
+        obj2.beforeTwelve = true
+      }
+
+      date.add(1, 'day')
       while (date.isBefore(endAt)) {
         dates.push(date.toDate())
+        bookHash[date.format('YYYY-MM-DD')] = { fullDay: true }
         date.add(1, 'day')
       }
+
       return dates
     }, []).sort((a,b)=> a - b);
+
+    run.schedule('afterRender', this, 'drawCalendarItems') // hack
+
+    return { dates, bookHash }
   }),
 
   days: computed('booking.startAt', 'booking.endAt', function() {
@@ -118,6 +164,7 @@ export default Ember.Controller.extend({
     setRange(range) {
       this.set('range', range)
       this.calculatePrice()
+      run.schedule('afterRender', this, 'drawCalendarItems') // hack
     },
 
     book() {
