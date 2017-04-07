63fb8b4bfaf319f12193d4da247c545b6efb3ca0
bekzod <bekzod@me.com>
2017-04-07 12:43:11 +0500

    sending header auth token for api requests

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,15 @@
 import DS from 'ember-data';
 
+const { computed, inject } = Ember
+
 export default DS.JSONAPIAdapter.extend({
   namespace: 'api',
-  // headers: Ember.computed('session.authToken',  function () {
-  //   return {
-  //     'x-authentication': this.get('session.authToken')
-  //   };
-  // });
+  session: inject.service(),
+
+  headers: computed('session.authToken',  function () {
+    return {
+      'x-authentication': this.get('session.authToken')
+    }
+  })
 
 });
--- mirage/config.js
+++ mirage/config.js
@@ -3,11 +3,12 @@ export default function() {
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
   this.namespace = '/api';
   this.get('/rentals');
+  this.post('/rentals');
   this.get('/rentals/:id');
+  this.del('/rentals/:id');
+
   this.get('/bookings');
   this.post('/bookings');
-    // this.post('/posts');
-    // this.get('/posts/:id');
-    // this.put('/posts/:id'); // or this.patch
-    // this.del('/posts/:id');
+  this.get('/bookings/:id');
+  this.del('/bookings/:id');
 }
